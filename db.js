68a461025e59fa828113e22aabcdcb9cd298c433
Tomomi Hobara <tomomihobra@hotmail.com>
2025-02-10 14:33:13 -0600

    2nd commit

---
+++ db.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+const db = mongoose.connection;
+
+// Set up the database
+
+mongoose.connect("mongodb+srv://tomh11:RcYDi1j67CdIOVBK@cluster0.qea7x.mongodb.net/SongDB?retryWrites=true&w=majority", {
+    // useNewUrlParser: true, // This is to deal with deprecation warnings
+    // useUnifiedTopology: true
+});
+
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+    console.log('Connected to the database');
+});
+
+module.exports = mongoose; // Export the mongoose module to be used in other files
+
+
+
+//m06user, password: UypwHOHm85czVkL3
---
+++ models/song.js
@@ -0,0 +1,13 @@
+const db = require("../db"); // Import the db.js file
+
+// Create a model for the songs
+const Song = db.model("Song", {
+    // There is a hidden parameter here called _id that is automatically created
+    title: {type: String, required: true},
+    artist: String,
+    popularity: {type:Number, min:1, max:10},
+    releaseDate: {type: Date, default: Date.now}, // Default is the current date
+    genre: [String] // This is an array of strings
+});
+
+module.exports = Song; // Export the Song model to be used in other files
